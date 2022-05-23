2c13cd3fee3741c5ac11af5ffcedb537cac38107
MohammadReza <md.mohammadreza+git@gmail.com>
2022-05-23 17:40:16 +0430

    update favicon

--- components/layout/Layout.tsx
+++ components/layout/Layout.tsx
@@ -13,7 +13,7 @@ const Layout: FC<{ children: PropsWithChildren<any>; pageTitle: string }> = ({
     <>
       <Head>
         <title>{pageTitle}</title>
-        <link rel="icon" href="/static/images/favicon.ico" />
+        <link rel="icon" href="/static/images/favicon.png" />
         <meta name="keywords" content="فروشگاه، خرید، لوازم جانبی" />
       </Head>
       <Header />
