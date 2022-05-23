533fb85ef4e3a0e68c92aa96f7bc440619817617
MohammadReza <md.mohammadreza+git@gmail.com>
2022-05-23 11:38:03 +0430

    add page 500

---
+++ pages/500.tsx
@@ -0,0 +1,28 @@
+import { Button, Box, Typography } from "@material-ui/core";
+import { useRouter } from "next/router";
+
+export default function Custom500() {
+  const Router = useRouter();
+
+  return (
+    <Box
+      display="flex"
+      justifyContent="center"
+      alignItems="center"
+      flexDirection="column"
+      height="80vh"
+    >
+      <Typography variant="subtitle1" gutterBottom>
+        در سمت سرور خطایی رخ داده است، دوباره تلاش کنید
+      </Typography>
+      <Button
+        size="small"
+        color="primary"
+        variant="contained"
+        onClick={() => Router.back()}
+      >
+        بازگشت به صفحه قبل
+      </Button>
+    </Box>
+  );
+}
