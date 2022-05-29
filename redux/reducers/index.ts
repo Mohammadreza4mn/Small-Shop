5984a062653a74f0e027e050d6ac28c9a56a9f6a
MohammadReza <md.mohammadreza+git@gmail.com>
2022-05-29 14:34:16 +0430

    remove redux-state-sync

--- redux/reducers/index.ts
+++ redux/reducers/index.ts
@@ -2,12 +2,9 @@ import { combineReducers } from "redux";
 import basket from "./basket";
 import toast from "./toast";
 import loading from "./loading";
-import { withReduxStateSync } from "redux-state-sync";
 
-const rootReducer = combineReducers({
+export default combineReducers({
   basket,
   toast,
   loading,
 });
-
-export default withReduxStateSync(rootReducer);
--- redux/store.ts
+++ redux/store.ts
@@ -3,11 +3,6 @@ import createSagaMiddleware, { Task } from "redux-saga";
 import rootReducer from "./reducers";
 import mySaga from "./sagas";
 import { createWrapper, Context } from "next-redux-wrapper";
-import {
-  createStateSyncMiddleware,
-  initMessageListener,
-} from "redux-state-sync";
-import { Basket } from "../utils/enum";
 
 interface SagaStore extends Store {
   sagaTask?: Task;
@@ -19,23 +14,11 @@ const bindMiddleware = (middleware: any) => {
 
 export const makeStore = (context?: Context) => {
   const sagaMiddleware = createSagaMiddleware();
-  const configSyncMiddleware = {
-    whitelist: [
-      Basket.updateBasketSuccess,
-      Basket.addBasketSuccess,
-      Basket.removeItemBasketSuccess,
-    ],
-  };
-
   const store: SagaStore = createStore(
     rootReducer,
-    bindMiddleware([
-      sagaMiddleware,
-      createStateSyncMiddleware(configSyncMiddleware),
-    ])
+    bindMiddleware([sagaMiddleware])
   );
 
-  initMessageListener(store);
   store.sagaTask = sagaMiddleware.run(mySaga);
 
   return store;
